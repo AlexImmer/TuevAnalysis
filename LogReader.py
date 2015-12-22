97e7a7dc875b5ac4b8977b21fa73e0c518f5f982
Alex Immer <alexander.immer@yahoo.com>
2015-12-23 00:35:06 +0100

    Add sensor data class

--- LogReader.py
+++ LogReader.py
@@ -1,3 +1,5 @@
+from SensorData import SensorData
+
 def read_acc_log(line):
     x_pos = line.find('x:')
     y_pos = line.find('y:')
@@ -16,6 +18,15 @@ def read_gps_log(line):
 def read_time_log(line):
     pos = line.find('Europe')-9
     return line[pos:pos+8]
+
+def avg_acc(d):
+    size = len(d)
+    if size == 0:
+        return 0, 0, 0
+    x = sum([x[0] for x in d])/size
+    y = sum([y[1] for y in d])/size
+    z = sum([z[2] for z in d])/size
+    return x, y, z
 
 def read_device_log(filename):
     res = {
@@ -39,3 +50,26 @@ def read_device_log(filename):
         elif line.startswith('D/CAAccTime'):
             res['time'].append(read_time_log(line))
     return res 
+
+def device_log_to_sensordata(filename):
+    sensordata = []
+    g_acc = []
+    alerts = []
+    gps = [0]
+    
+    for line in open(filename):
+        if line.startswith('D/CAAccData'):
+            g_acc.append(read_acc_log(line))
+        elif line.startswith('I/CrashAlert'):
+            alerts.append(read_alert_log(line))
+        elif line.startswith('D/CrashAlertGPS'):
+            gps.append(read_gps_log(line))
+        elif line.startswith('D/CAAccTime'):
+            time = read_time_log(line)
+            accs = avg_acc(g_acc)
+            sd = SensorData(accs[0], accs[1], accs[2], gps[-1], time, alerts)
+            sensordata.append(sd)
+            g_acc = []
+            alerts = []
+    
+    return sensordata
--- SensorData.py
+++ SensorData.py
@@ -1,8 +1,24 @@
+import numpy as np
+
 class SensorData(object):
     def __init__(self, x, y, z, gps, time, alert):
         self.x = x
         self.y = y
         self.z = z
         self.speed = gps
-        self.time = time
+        self.time = self.to_secs(time)
         self.alerts = alert
+        
+    def gyro_force(self):
+        rms = self.x ** 2 + self.y ** 2 + self.z ** 2
+        return np.sqrt(rms)
+    
+    def gyro_sum(self):
+        return self.x + self.y + self.z
+    
+    @classmethod
+    def to_secs(cls, t):
+        h = int(t[:2])
+        m = int(t[3:5])
+        s = int(t[6:])
+        return 60 ** 2 * h + 60 * m + s
